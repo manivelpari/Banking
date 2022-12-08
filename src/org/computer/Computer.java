7fb42b6f5efb98bf223000358f8650c8f2b48400
manivelpari <manivelpari7@gmail.com>
2022-12-08 12:26:31 +0530

    First Pull by Pari

--- src/org/computer/Computer.java
+++ src/org/computer/Computer.java
@@ -3,7 +3,12 @@ package org.computer;
 public class Computer {
 	public void compputerModel() {
 	System.out.println("Computer Model : Intel core 8th generation");
-
 	}
-
+	private void laptop() {
+		System.out.println("Laptop Model: Dell");
+	}
+	
+	private void tab() {
+		System.out.println("Tab Name: Apple");
+	}
 }
